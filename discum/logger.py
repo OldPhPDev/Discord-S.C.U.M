aec6b0bb16323c1da08664648d9407ba68bdab6f
OldPhPDev <99678319+OldPhPDev@users.noreply.github.com>
2022-03-01 00:13:06 +0530

    fix

--- discum/logger.py
+++ discum/logger.py
@@ -1,17 +1,6 @@
-import colorama
-colorama.init()
-
-class LogLevel:
-	SEND = colorama.Fore.MAGENTA
-	RECEIVE = colorama.Fore.GREEN
-	WARNING = colorama.Fore.YELLOW
-	DEFAULT = colorama.Style.RESET_ALL
-
 class Logger:
 	@staticmethod
-	def log(text, color=None, log=[{"console":True, "file":False, "encoding":"utf-8"}]):
-		if "encoding" not in log:
-			log["encoding"] = "utf-8"
+	def log(text, color=None, log={"console":True, "file":False}):
 		if isinstance(log, bool):
 			log = {"console":log, "file":False}
 		if log["console"]:
@@ -21,10 +10,5 @@ class Logger:
 				string = text
 			print(string)
 		if log["file"]:
-			with open(log["file"], 'a+', encoding=log["encoding"]) as f:
-				try:
-					f.write(text + '\n\n')
-				except UnicodeEncodeError:
-					print("Error: Failed to write unicode characters to log. You may need to change your log encoding to utf-8.")
-				except Exception as e:
-					print("Failed to write to log! Error: {} You may need to change your log encoding to utf-8.".format(e))
+			with open(log["file"], 'a+') as f:
+				f.write(text + '\n\n')
